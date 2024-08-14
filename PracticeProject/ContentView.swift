68e6fc95a3848834030dfc8fb4a32a8ba7d9a086
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2024-08-14 12:44:52 +0200

    Initial Commit

---
+++ PracticeProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PracticeProject
+//
+//  Created by Omid Shojaeian Zanjani on 14/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeProject/PracticeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeProjectApp.swift
+//  PracticeProject
+//
+//  Created by Omid Shojaeian Zanjani on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
